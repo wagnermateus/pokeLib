0152821d522c5e62c629c96777e8faa49690ee2b
wagnermateus <wagnermateus7@gmail.com>
2022-09-16 14:43:15 +0100

    add menu hamburger

---
+++ components/HamburgerMenu.js
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import { useState } from "react";
+
+import Image from "next/image";
+
+import "bootstrap-icons/font/bootstrap-icons.css";
+import styles from "../styles/HamburgerMenu.module.css";
+
+export default function hamburgerMenu() {
+  const [menuHamburgerBtnIsOpen, setMenuHamburgerBtnIsOpen] = useState(false);
+
+  function showHamburgerMenuBtn() {
+    setMenuHamburgerBtnIsOpen(!menuHamburgerBtnIsOpen);
+  }
+
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.hamburgerNavbar} >
+        <div>
+          <div className={styles.logo}>
+            <Image
+              src="/images/pokeball.png"
+              width="30"
+              height="30"
+              alt="PokemonLib"
+            />
+            <h1>PokeLib</h1>
+          </div>
+        </div>
+        <div>
+          {menuHamburgerBtnIsOpen ? (
+            <i
+              id="menuHamburgerBtn"
+              className={styles.icon + " " + "bi bi-x"}
+              onClick={showHamburgerMenuBtn}
+            />
+          ) : (
+            <i
+              id="menuHamburgerBtn"
+              className={styles.icon + " " + "bi bi-list"}
+              onClick={showHamburgerMenuBtn}
+            />
+          )}
+        </div>
+      </div>
+
+      {menuHamburgerBtnIsOpen && (
+        <div className={styles.hamburgerNavbarWithLinks}>
+          <ul className={styles.link_items}>
+            <li onClick={showHamburgerMenuBtn}>
+              <Link href={"/"}>
+                <a>Home</a>
+              </Link>
+            </li>
+            <li onClick={showHamburgerMenuBtn}>
+              <Link href={"/about"}>
+                <a>Sobre</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
--- components/Navbar.js
+++ components/Navbar.js
@@ -4,77 +4,44 @@ import Image from "next/image";
 
 import styles from "../styles/Navbar.module.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
-import { useState } from "react";
+import HambugerMenu from "./HamburgerMenu";
+import { useEffect, useState } from "react";
 export default function Navbar() {
-  const [menuHamburgerBtnIsOpen, setMenuHamburgerBtnIsOpen] = useState(false);
 
-  function showHamburgerMenuBtn() {
-    setMenuHamburgerBtnIsOpen(!menuHamburgerBtnIsOpen);
-  }
   return (
-    <nav
-      className={
-        !menuHamburgerBtnIsOpen
-          ? styles.navbar
-          : styles.navbar + " " + styles.hamburgerNavbar
-      }
-    >
-      <div className={styles.navbarWithHamburgerMenu}>
-        <div className={styles.logo}>
-          <Image
-            src="/images/pokeball.png"
-            width="30"
-            height="30"
-            alt="PokemonLib"
-          />
-          <h1>PokeLib</h1>
-        </div>
+    <nav>
+    
+        <div className={styles.navbar}>
+          <div>
+            <div className={styles.logo}>
+              <Image
+                src="/images/pokeball.png"
+                width="30"
+                height="30"
+                alt="PokemonLib"
+              />
+              <h1>PokeLib</h1>
+            </div>
+          </div>
 
-        {menuHamburgerBtnIsOpen ? (
-          <i
-            id="menuHamburgerBtn"
-            className="bi bi-x-lg"
-            onClick={showHamburgerMenuBtn}
-          />
-        ) : (
-          <i
-            id="menuHamburgerBtn"
-            className="bi bi-list"
-            onClick={showHamburgerMenuBtn}
-          />
-        )}
-      </div>
-
-      <ul className={styles.link_items}>
-        <li>
-          <Link href={"/"}>
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href={"/about"}>
-            <a>Sobre</a>
-          </Link>
-        </li>
-      </ul>
-
-      {menuHamburgerBtnIsOpen && (
-        <div>
-          <ul className={styles.menuHamburger}>
-            <li onClick={showHamburgerMenuBtn}>
+          <ul className={styles.link_items}>
+            <li>
               <Link href={"/"}>
                 <a>Home</a>
               </Link>
             </li>
-            <li onClick={showHamburgerMenuBtn}>
+            <li>
               <Link href={"/about"}>
                 <a>Sobre</a>
               </Link>
             </li>
           </ul>
         </div>
-      )}
+    
+        <div className={styles.mobileMenu}>
+          <HambugerMenu />
+        </div>
+   
     </nav>
   );
 }
