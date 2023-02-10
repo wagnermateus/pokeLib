b7c1b2549cc61d7114a33bd6d122293e7d694ed2
wagnermateus <wagnermateus7@gmail.com>
2023-02-10 11:33:16 +0100

    correct in the @ of the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,16 @@
 import styles from "../styles/Footer.module.css";
-export default function Footer(){
-    return(
-        <footer className={styles.footer}>
-            <p>
-                <span>PokeLib </span> &copy;2022 
-                
-            </p>
-           <p>Devloped by <span><i>@WagnerMateus</i></span> </p> 
-        </footer>
-    );
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>PokeLib </span> &copy;2022
+      </p>
+      <p>
+        Developed by{" "}
+        <span>
+          <i>@WagnerMateus</i>
+        </span>{" "}
+      </p>
+    </footer>
+  );
 }
